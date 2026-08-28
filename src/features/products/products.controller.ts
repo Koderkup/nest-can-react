70bd02aad60539899f1e12de9b04f50c464c70a8
Muhammad Atif <muhammad.atif@unitedsol.net>
2026-08-28 10:44:06 +0500

    Implement product retrieval features in ProductsController and ProductsService. Added endpoints to get a product by ID and to query products by name and price. Refactored product data handling in ProductsService for better organization.

--- src/features/products/products.controller.ts
+++ src/features/products/products.controller.ts
@@ -1,14 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { ProductsService } from '@/features/products/products.service';
 
 @Controller('products')
 export class ProductsController {
-  constructor(
-    private readonly productsService: ProductsService,
-  ) {}
+  constructor(private readonly productsService: ProductsService) {}
 
   @Get()
   getProducts() {
     return this.productsService.getProducts();
+  }
+  @Get(':id')
+  getProductById(@Param('id') id: string) {
+    return this.productsService.getProductById(id);
+  }
+  @Get()
+  getProduct(@Query('name') name: string, @Query('price') price: number) {
+    return this.productsService.getProduct(name, price);
   }
 }
--- src/features/products/products.service.ts
+++ src/features/products/products.service.ts
@@ -1,21 +1,39 @@
 import { Injectable } from '@nestjs/common';
-
+const products = [
+  {
+    id: 1,
+    name: 'iPhone',
+    price: 1000,
+  },
+  {
+    id: 2,
+    name: 'MacBook',
+    price: 2000,
+  },
+  {
+    id: 3,
+    name: 'iPad',
+    price: 3000,
+  },
+  {
+    id: 4,
+    name: 'iPod',
+    price: 4000,
+  },
+];
 @Injectable()
 export class ProductsService {
-  private readonly products = [
-    {
-      id: 1,
-      name: 'iPhone',
-      price: 1000,
-    },
-    {
-      id: 2,
-      name: 'MacBook',
-      price: 2000,
-    },
-  ];
+  private readonly products = products;
 
   getProducts() {
+    console.log('getProducts');
     return this.products;
+  }
+  getProductById(id: string) {
+    return this.products.find((p) => p.id === parseInt(id));
+  }
+  getProduct(name: string, price: number) {
+    console.log(name, price);
+    return this.products.find((p) => p.name === name && p.price === price);
   }
 }
